fa15540c41d00b0542f6596df896437fcd2d00df
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-05-17 08:11:39 +0530

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,18 @@
 // our-domain.com/news
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 export default function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/headline">Next js framework</Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
 }
